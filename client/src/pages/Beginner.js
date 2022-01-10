18314753bd981994e2c07bde857b16b0b4de70ec
MuhammadAmirVohra <k180124@nu.edu.pk>
2022-01-10 11:21:25 +0500

    timer

--- client/src/pages/Beginner.js
+++ client/src/pages/Beginner.js
@@ -2,21 +2,28 @@ import React from "react";
 import Header from "../Common Components/Navbar";
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import { Button } from "react-bootstrap";
-// import Timer from "./Timer";
+import Timer from "./Timer";
+
 const Beginner = () => {
     const [questions, setQuestions] = useState([]);
     useEffect(() => {
         axios.get("http://localhost:5000/api/questions").then((Response) => {
+            console.log(Response.data.questions);
             setQuestions(Response.data.questions);
         });
     }, []);
+
+
     return (
         <>
             <Header />
-            {questions.map((q) => {
+            <Timer initialMinute={2} initialSeconds={59} />
+            {questions.map((q, idx) => {
 
-                return (<h1>{q}</h1>)
+                return (
+                    <h1 key={idx}>{q}</h1>
+
+                )
 
 
             })
--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import { useTime } from 'react-timer-hook';
-const Timer = () => {
-    const {
-        seconds,
-        minutes,
-        hours,
-        ampm,
-    } = useTime({ format: '12-hour' });
+import { useState, useEffect } from "react";
+
+const Timer = (props) => {
+    const { initialMinute = 0, initialSeconds = 0 } = props;
+    const [minutes, setMinutes] = useState(initialMinute);
+    const [seconds, setSeconds] = useState(initialSeconds);
+    useEffect(() => {
+        let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(myInterval)
+                } else {
+                    setMinutes(minutes - 1);
+                    setSeconds(59);
+                }
+            }
+        }, 1000)
+        return () => {
+            clearInterval(myInterval);
+        };
+    });
 
     return (
-        <>
-            <div style={{ textAlign: 'center' }}>
-                {/* <h1>react-timer-hook </h1> */}
-                {/* <p>Current Time Demo</p> */}
-                <div style={{ fontSize: '40px' }}>
-                    <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span><span>{ampm}</span>
-                </div>
-            </div>
-        </>
-    );
+        <div>
+            {minutes === 0 && seconds === 0
+                ? null
+                : <h1> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+            }
+        </div>
+    )
 }
 
 export default Timer;
